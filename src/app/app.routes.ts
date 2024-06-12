ffc2cbe929c2f90d5c4f857c7db0c56e2fba38d7
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-06-12 15:10:43 -0500

    version 1.0.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+//import { Routes } from '@angular/router';
+//export const routes: Routes = [];
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RealizarTestComponent } from './realizar-test/realizar-test.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'test', component: RealizarTestComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
