b9db7127a92ab65b7947bbf1c98fafa1fbad0f91
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-06-12 19:54:55 -0500

    version 1.1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,14 @@
-//import { Routes } from '@angular/router';
-//export const routes: Routes = [];
-
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RealizarTestComponent } from './realizar-test/realizar-test.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RealizarTestComponent } from './component/realizar-test/realizar-test.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'test', component: RealizarTestComponent },
+  { path: 'realizar-test', component: RealizarTestComponent }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// Exportación de las rutas
+export { routes };
