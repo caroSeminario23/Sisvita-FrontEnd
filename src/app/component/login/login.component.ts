7b21ff48c8ce973b6fe81c739baf2900c4abb892
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-06-13 09:24:03 -0500

    version 1.1.2

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2'; // Importa SweetAlert2
 
 @Component({
   selector: 'app-login',
@@ -8,38 +9,42 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  userType: string | undefined;
-  email: string | undefined;
-  password: string | undefined;
-  errorMessage: string | undefined;
+  user_type: string ='';
+  email: string ='';
+  contrasenia: string ='';
+  errorMessage: string ='';
 
   constructor(private authService: AuthService, private router: Router) { }
 
   onSubmit() {
-    if (this.userType && this.email && this.password) {
-      this.authService.login(this.userType, this.email, this.password).subscribe(
+    if (this.user_type && this.email && this.contrasenia) {
+      this.authService.login(this.user_type, this.email, this.contrasenia).subscribe(
         response => {
           if (response.success) {
-            this.redirectUser(response.userType);
+            this.redirectUser(response.user_type);
+            Swal.fire('¡Éxito!', 'Inicio de sesión exitoso', 'success'); // Muestra SweetAlert2 de éxito
           } else {
             this.errorMessage = response.message;
+            Swal.fire('¡Error!', this.errorMessage, 'error'); // Muestra SweetAlert2 de error
           }
         },
         error => {
           this.errorMessage = 'Error de servidor. Por favor, intente de nuevo más tarde.';
+          Swal.fire('¡Error!', this.errorMessage, 'error'); // Muestra SweetAlert2 de error
         }
       );
     } else {
       this.errorMessage = 'Por favor, complete todos los campos.';
+      Swal.fire('¡Advertencia!', this.errorMessage, 'warning'); // Muestra SweetAlert2 de advertencia
     }
   }
 
   redirectUser(userType: string) {
-    if (userType === 'student') {
+    if (userType === 'Estudiante') {
       this.router.navigate(['/student-dashboard']);
-    } else if (userType === 'specialist') {
+    } else if (userType === 'Especialista') {
       this.router.navigate(['/specialist-dashboard']);
-    } else if (userType === 'admin') {
+    } else if (userType === 'Administrador') {
       this.router.navigate(['/admin-dashboard']);
     }
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private baseUrl = 'https://sisvita2.onrender.com';
+  private baseUrl = 'http://127.0.0.1:5000/cus_routes/login'; // Cambia esto por la URL de tu servidor Flask
 
   constructor(private http: HttpClient) { }
 
   login(userType: string, email: string, password: string): Observable<any> {
-    return this.http.post(`${this.baseUrl}/auth/login`, { userType, email, password });
+    const body = { email, contrasenia: password };
+
+    return this.http.post<any>(this.baseUrl, body).pipe(
+      catchError(error => {
+        throw 'Error en la autenticación: ' + error.message;
+      })
+    );
   }
 }
