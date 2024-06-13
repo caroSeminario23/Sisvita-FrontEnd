0a9785e54e1ae74e08f93fc5417ab3b7fb74659f
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-06-12 20:42:04 -0500

    version 1.1.1

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  userType: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+  errorMessage: string | undefined;
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    if (this.userType && this.email && this.password) {
+      this.authService.login(this.userType, this.email, this.password).subscribe(
+        response => {
+          if (response.success) {
+            this.redirectUser(response.userType);
+          } else {
+            this.errorMessage = response.message;
+          }
+        },
+        error => {
+          this.errorMessage = 'Error de servidor. Por favor, intente de nuevo más tarde.';
+        }
+      );
+    } else {
+      this.errorMessage = 'Por favor, complete todos los campos.';
+    }
+  }
+
+  redirectUser(userType: string) {
+    if (userType === 'student') {
+      this.router.navigate(['/student-dashboard']);
+    } else if (userType === 'specialist') {
+      this.router.navigate(['/specialist-dashboard']);
+    } else if (userType === 'admin') {
+      this.router.navigate(['/admin-dashboard']);
+    }
+  }
 }
